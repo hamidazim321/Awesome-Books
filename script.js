23d357988085bc7896cccd9b2410d228d8d5eb9e
suleiman gacheru <dretrevor8@gmail.com>
2023-07-04 11:58:36 +0300

    fix linters

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-const books = JSON.parse(localStorage.getItem('books')) || [];
-
+let books = JSON.parse(localStorage.getItem('books')) || [];
 function renderBookList() {
   const bookList = document.getElementById('bookList');
   bookList.innerHTML = '';
-
   const reversedBooks = books.slice().reverse();
-
   reversedBooks.forEach((book) => {
     const listItem = document.createElement('li');
     listItem.textContent = `${book.title} by ${book.author}`;
@@ -13,7 +10,9 @@ function renderBookList() {
     const removeButton = document.createElement('button');
     removeButton.textContent = 'Remove';
     removeButton.addEventListener('click', () => {
-      removeBook(book);
+      books = books.filter((b) => b !== book);
+      localStorage.setItem('books', JSON.stringify(books));
+      renderBookList();
     });
 
     listItem.appendChild(removeButton);
@@ -21,31 +20,23 @@ function renderBookList() {
   });
 }
 
-  function addBook(title, author) {
-    var newBook = { title: title, author: author };
-    books.push(newBook);
-    localStorage.setItem("books", JSON.stringify(books));
-    renderBookList();
-  }
-
-  function removeBook(book) {
-    books = books.filter(function(b) {
-      return b !== book;
-    });
-    localStorage.setItem("books", JSON.stringify(books));
-    renderBookList();
-  }
-
-  var addForm = document.getElementById("addForm");
-  addForm.addEventListener("submit", function(event) {
-    event.preventDefault();
-    var titleInput = document.getElementById("titleInput");
-    var authorInput = document.getElementById("authorInput");
-    var title = titleInput.value;
-    var author = authorInput.value;
-    addBook(title, author);
-    titleInput.value = "";
-    authorInput.value = "";
-  });
-
+function addBook(title, author) {
+  const newBook = { title, author };
+  books.push(newBook);
+  localStorage.setItem('books', JSON.stringify(books));
   renderBookList();
+}
+
+const addForm = document.getElementById('addForm');
+addForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const titleInput = document.getElementById('titleInput');
+  const authorInput = document.getElementById('authorInput');
+  const title = titleInput.value;
+  const author = authorInput.value;
+  addBook(title, author);
+  titleInput.value = '';
+  authorInput.value = '';
+});
+
+renderBookList();
