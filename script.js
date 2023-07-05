912b45e09e4247be911203b032467bc47150162d
suleiman yusuf <108791588+hetrox8@users.noreply.github.com>
2023-07-05 18:13:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,48 @@ class BookManager {
     BookManager.displayBooks(storedBooks);
   }
 }
+
+  static removeBook(i) {
+    const storedBooks = BookManager.getStoredBooks();
+    storedBooks.splice(i, 1);
+    BookManager.updateStoredBooks(storedBooks);
+    BookManager.displayBooks();
+  }
+
+  static createBookListHTML(books) {
+    let bookListHTML = '';
+    for (let i = 0; i < books.length; i += 1) {
+      const { title, author } = books[i];
+      bookListHTML += `
+      <div class="book-list">
+        <p>"${title}" by "${author}"</p>
+        <button onClick="BookManager.removeBook(${i})">Remove</button>
+      </div>
+      `;
+    }
+    return bookListHTML;
+  }
+
+  static displayBooks() {
+    const bookContainer = document.querySelector('.book-container');
+    const storedBooks = BookManager.getStoredBooks();
+    const bookListHTML = BookManager.createBookListHTML(storedBooks);
+    bookContainer.innerHTML = `
+      <ul class="book-ul">${bookListHTML}</ul>
+    `;
+  }
+}
+
+const bookForm = document.querySelector('.book-form');
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const bookTitleInput = document.querySelector('.book-title');
+  const bookAuthorInput = document.querySelector('.book-author');
+  const bookTitle = bookTitleInput.value;
+  const bookAuthor = bookAuthorInput.value;
+  BookManager.addNewBook(bookTitle, bookAuthor);
+  bookTitleInput.value = '';
+  bookAuthorInput.value = '';
+});
+
+BookManager.displayBooks();
